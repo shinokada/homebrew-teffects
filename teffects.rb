85c0ff66fba41d2b655ac7683637e3cac6b6fd0a
Shinichi Okada <okada.shin@gmail.com>
2021-10-24 20:45:34 +0900

    version 1.0.6

--- teffects.rb
+++ teffects.rb
@@ -1,7 +1,7 @@
 class Teffects < Formula
     desc "Teffects is a command-line text effects library."
-    url "https://github.com/shinokada/teffects/archive/refs/tags/v1.0.4.tar.gz"
-    sha256 "1aa64d34e0ad70721417fdc86b1de8c8320d6b81b1c8d22bb2088f147027813d"
+    url "https://github.com/shinokada/teffects/archive/refs/tags/v1.0.6.tar.gz"
+    sha256 "4ca94067a552f210357c3a118af474475e73add39e19d855917b90f807d13422"
     license "MIT"
 
     def install
